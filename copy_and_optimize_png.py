915d79fa519de9c429366f9af1d56c856fbb6851
drojf <drojfjord@gmail.com>
2022-09-03 11:59:45 +1000

    Add support for .jpg files, just copy non-optimizeable files

--- copy_and_optimize_png.py
+++ copy_and_optimize_png.py
@@ -6,15 +6,18 @@ import sys
 from PIL import Image, ImageChops
 from pathlib import Path
 
+# These formats must be both openable by PIL and optimizeable by ect
+optimizable_formats = ['.png', '.jpg', '.jpeg']
 
 class CopyingReport:
 	def __init__(self):
 		self.dst_update = 0
 		self.new_file = 0
 		self.same_content = 0
+		self.non_optimizeable = 0
 
 	def validate(self, expected_count):
-		return expected_count == (self.dst_update + self.new_file + self.same_content)
+		return expected_count == (self.dst_update + self.new_file + self.same_content + self.non_optimizeable)
 
 
 # https://github.com/shssoichiro/oxipng
@@ -38,23 +41,27 @@ def images_are_same(src_path, dst_path) -> bool:
 
 	return diff.getbbox() == None
 
-
-def image_needs_update(src_path, dst_path, report: CopyingReport) -> bool:
-	if not os.path.exists(dst_path):
+# Returns a tuple of (needs_copy, needs_optimize)
+def image_needs_update(src_path: Path, dst_path, report: CopyingReport) -> bool:
+	if src_path.suffix.lower() not in optimizable_formats:
+		print(f"  > Warning: detected non-optmizable/non-comparable file {src_path} - will overwrite {dst_path} even if same")
+		report.non_optimizeable += 1
+		return True, False
+	elif not os.path.exists(dst_path):
 		print(f"  > Warning: new file detected {src_path} as no file at {dst_path}")
 		report.new_file += 1
-		return True
+		return True, True
 	elif not images_are_same(src_path, dst_path):
 		print("  > Image is different - copying and optimizing with ect")
 		report.dst_update += 1
-		return True
+		return True, True
 	else:
 		report.same_content += 1
-		return False
+		return False, False
 
 
 def main(src_folder, dst_folder):
-	src_paths = list(Path(src_folder).rglob('**/*.png'))
+	src_paths = list(Path(src_folder).rglob('**/*.*'))
 
 	copying_report = CopyingReport()
 	for src_path in src_paths:
@@ -62,15 +69,19 @@ def main(src_folder, dst_folder):
 		dst_path = os.path.join(dst_folder, src_rel_path)
 
 		print(f"Processing [{src_rel_path}]")
-		if image_needs_update(src_path, dst_path, copying_report):
+		need_copy, need_optimize = image_needs_update(src_path, dst_path, copying_report)
+		if need_copy:
 			Path(os.path.dirname(dst_path)).mkdir(parents=True, exist_ok=True)
 			shutil.copyfile(src_path, dst_path)
-			ect(dst_path)
+
+			if need_optimize:
+				ect(dst_path)
 
 	print(f"Got {len(src_paths)} src images")
 	print(f"Updated: {copying_report.dst_update}")
 	print(f"New    : {copying_report.new_file}")
 	print(f"Same   : {copying_report.same_content}")
+	print(f"No Opt : {copying_report.non_optimizeable}")
 
 	if not copying_report.validate(len(src_paths)):
 		raise Exception("ERROR: number of files processed doesn't match expected num files!")
